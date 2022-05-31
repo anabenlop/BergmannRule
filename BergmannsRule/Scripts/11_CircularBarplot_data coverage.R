a9943866607a9c93d700b2ebb7fb036e90237e76
anabenlop <abenitez81@gmail.com>
2022-05-31 22:15:40 +0200

    Update 11_CircularBarplot_data coverage.R

--- BergmannsRule/Scripts/11_CircularBarplot_data coverage.R
+++ BergmannsRule/Scripts/11_CircularBarplot_data coverage.R
@@ -23,6 +23,7 @@ library(phytools)
 
 # Phylogenetic trees for the four taxa ####
 # mammals ####
+# load mammals data from PHYLACINE
 ET<-read.csv('Data/EltonTraits_Mammals_taxid.csv', stringsAsFactors = F)
 ET<-ET[!ET$Order %in% c('Sirenia'),]
 ET<-ET[!ET$MSWFamilyLatin %in% c('Octodontidae','Otariidae','Balaenidae','Balaenopteridae','Ziphiidae','Neobalaenidae','Delphinidae','Phocidae','Monodontidae','Dugongidae','Iniidae','Physeteridae','Phocoenidae','Odobenidae','Trichechidae', 'Platanistidae'),]
@@ -178,6 +179,8 @@ b_tree2 <- drop.tip(b_tree, tip = c("Alcidae","Chionidae", "Diomedeidae", "Frega
 # join dataset with families included in Bergmann's rule paper
 bfam <- left_join(bfam, bird_tavg, by = "family")
 bfam$inc <- ifelse(is.na(bfam$mean), "no", "yes")
+bfam$bin <- ifelse(bfam$inc == "yes", 1, 0)
+bfam$bin <- as.integer(bfam$bin)
 bfam$hyp <- ifelse(bfam$mean < 0, "yes","no")
 
 # palette <- c("yes" = "dark purple", "no" = "yellow")
@@ -210,19 +213,33 @@ p2 %<+% bfam2 + geom_tree(aes(color = mean), size = 1) + geom_tiplab(aes(color =
 p2 %<+% bfam2 + geom_tree(aes(color = hyp), size = 1) + geom_tiplab(aes(color = hyp)) +
          scale_color_manual(values= wes_palette("Cavalcanti1", n = 2)) 
 
-table(bfam2$bergmann) # 104 families follow bergmann's rule
+ggsave("Figures/phylo_birds_tavg.png", 
+       width= 250, height= 250, units = 'mm', dpi=300)
 
 
-# phylogenetic clustering
+table(bfam2$hyp) # 104 families follow bergmann's rule
 
-bfam$bin <- ifelse(bfam$inc == "yes", 1, 0)
-bfam$bin <- as.integer(bfam$bin)
+# Test phylogenetic clustering of included avian families. Are most included families related?
 
-bfam_D <- comparative.data(b_tree2, bfam, family) # does not work, it only shows families included in the analysis.
+# The D-statistic is the sum of state changes along the edges of a phylogeny. 
+# The D statistic is equal to 1 if the observed binary trait has a phylogenetically random distribution across 
+# the tips of the phylogeny and to 0 if the observed trait is as clumped as if it had evolved by Brownian motion under 
+# our threshold model. Values of D can fall outside this range. 
+# Continuous vertical lines represent the mean of random D-statistic values obtained from two null models: 
+# Brownian motion (red) and phylogenetic randomness (blue). 
+# Dashed gray lines represent observed D-statistics.
+
+bfam_D <- comparative.data(b_tree2, bfam[,c("family","bin")], family)
 PhyloD <- phylo.d(bfam_D, binvar=bin)
+plot(PhyloD) # some phylogenetic clumping, but nothing too severe 
 
-# reptiles ####
-ET<-read.csv('Data/reptile_checklist_2022_03.csv', stringsAsFactors = F)
+# Test phylogenetic clustering of families adhering to the heat conservation hypothesis. Are the majority of families that follow the heat conserv hyp clustered?
+bfam_D2 <- comparative.data(b_tree3, bfam2[,c("family","hyp")], family) 
+PhyloD2 <- phylo.d(bfam_D2, binvar = hyp)
+plot(PhyloD2) # phylogenetically random pattern, almost overdispersed across families.
+
+## reptiles ####
+rept_tax <- read.csv('Data/reptile_checklist_2022_03.csv', stringsAsFactors = F)
 
 reptdata <- read.csv('Data/reptdata_ph.csv', stringsAsFactors = F)
 reptdata <- reptdata[reptdata$env.var == 'tavg', ]
@@ -230,105 +247,16 @@ reptdata <- reptdata[reptdata$env.var == 'tavg', ]
 ET2<-ET[!duplicated(ET$BLFamilyLatin),] # 184 families
 reptdata2<-reptdata[!duplicated(reptdata$family),] # 140 families (140/184*100 = 76%)
 
-TabET<-table(ET$IOCOrder) #n species
-TabETf<-table(ET2$IOCOrder) #n families
-TabSP<-table(reptdata$order) #n species
-TabSPf<-table(reptdata2$order) #n families
+r_tree <- ape::read.tree(file = "Data/rep_phyl_Pyron.txt")
+r_tree 
 
-ET<-data.frame(Order=names(TabET), Nfm=as.numeric(TabETf), Nsps=as.numeric(TabET))
-rept_dt<-data.frame(Order=names(TabSP), NfmInSamp=as.numeric(TabSPf), NspInSamp= as.numeric(TabSP))
-
-data<-left_join(ET, rept_dt, by = 'Order')
-data$NspInSamp[is.na(data$NspInSamp)]<-0
-data$NfmInSamp[is.na(data$NfmInSamp)]<-0
-
-data$Percentage<-round(100*data$NspInSamp/data$Nsps)
-data$PercentageMissing<-100-data$Percentage
-data$PercentageFm<-round(100*data$NfmInSamp/data$Nfm)
-data$PercentageFm<-ifelse(data$PercentageFm>100, 100, data$PercentageFm)
-data$PercentageMissingFm<-100-data$PercentageFm
-
-data2 <- data
-
-#PERCENTAGE OF FAMILIES
-data2$labelFam<-paste0(data2$Order)
-
-data2 <- as.data.frame(pivot_longer(data, cols=c(PercentageFm, PercentageMissingFm), names_to = "Var", values_to = "Value"))
-
-data2$id<-as.numeric(as.factor(data2$Order))
-data2$angle= 90 - 360 * (data2$id-0.5) / length(unique(data2$id))     # I substract 0.5 because the letter must have the angle of the center of the bars. Not extreme right(1) or extreme left (0)
-data2$Var<-factor(data2$Var, levels=c('PercentageMissingFm','PercentageFm'))
-data2$hjust<-ifelse(data2$angle < -90, 1, 0)
-data2$angle<-ifelse(data2$angle < -90, data2$angle+180, data2$angle)
-data2$LabNum<-ifelse(data2$Var =='PercentageFm', data2$Value, '')
-data2$NfmInSamp<-ifelse(data2$NfmInSamp>data2$Nfm, data2$Nfm, data2$NfmInSamp)
-data2$Lab2<-ifelse(data2$Var =='PercentageFm', paste0(' ', data2$LabNum, '% (',data2$NfmInSamp,'/',data2$Nfm,') '), '')
-#data2[data2$Var=='Nsps', 'Value']<-100
-#data2<-data2[data2$Var!='NspInSamp',]
-
-# Create dataset
-p1<-ggplot(data2) +      
-  # Add the stacked bar
-  geom_bar(aes(x=Order, y=Value, fill=Var), stat="identity", alpha=0.5) +
-  # scale_fill_viridis(discrete=TRUE, direction=-1) +  
-  scale_fill_manual(values = c("#a6cee3", "#1f78b4")) +
-  theme_minimal() +
-  theme(
-    legend.position = "none",
-    axis.text = element_blank(),
-    axis.title = element_blank(),
-    panel.grid = element_blank(),
-    plot.margin = unit(c(1,0,0,1), "cm")      
-  ) +
-  coord_polar(clip='off') +
-  # Add labels on top of each bar
-  geom_text(aes(x=id, y=40, label=Order, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=4, angle= data2$angle, inherit.aes = FALSE) +
-  geom_text(aes(x=id, y=100, label=Lab2, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=4.2, angle= data2$angle, inherit.aes = FALSE )
-p1
-
-#PERCENTAGE OF SPECIES
-data2 <- as.data.frame(pivot_longer(data, cols=c(Percentage, PercentageMissing), names_to = "Var", values_to = "Value"))
-
-data2$id<-as.numeric(as.factor(data2$Order))
-data2$angle= 90 - 360 * (data2$id-0.5) / length(unique(data2$id))     # I substract 0.5 because the letter must have the angle of the center of the bars. Not extreme right(1) or extreme left (0)
-data2$Var<-factor(data2$Var, levels=c('PercentageMissing','Percentage'))
-data2$hjust<-ifelse(data2$angle < -90, 1, 0)
-data2$angle<-ifelse(data2$angle < -90, data2$angle+180, data2$angle)
-
-data2$LabNum<-ifelse(data2$Var =='Percentage', data2$Value, '')
-data2$Lab2<-ifelse(data2$Var =='Percentage', paste0(' ',data2$LabNum, '% (',data2$NspInSamp,'/',data2$Nsps,') '), '')
-
-# Create dataset
-p2<-ggplot(data2) +      
-  # Add the stacked bar
-  geom_bar(aes(x=Order, y=Value, fill=Var), stat="identity", alpha=0.5) +
-  #scale_fill_viridis(discrete=TRUE, direction=-1) +  
-  scale_fill_manual(values = c("#a6cee3", "#1f78b4")) +
-  theme_minimal() +
-  theme(
-    legend.position = "none",
-    axis.text = element_blank(),
-    axis.title = element_blank(),
-    panel.grid = element_blank(),
-    plot.margin = unit(c(1,0,0,1), "cm")      
-  ) +
-  coord_polar(clip='off') +
-  # Add labels on top of each bar
-  geom_text(aes(x=id, y=40, label=Order, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=4, angle= data2$angle, inherit.aes = FALSE) +
-  geom_text(aes(x=id, y=100, label=Lab2, hjust=hjust), color="black", fontface="bold",alpha=0.6, size=4.2, angle= data2$angle, inherit.aes = FALSE )
-p2
-
-#SAVE ALL
-pdf('Figures/FamilyCoverageRept.pdf', width=18, height=12)#width=13, height=8)
-ggarrange(p2, p1, labels=c('(a)', '(b)'), font.label=list(size=20, face='plain'),
-          nrow=2, ncol=2)
-dev.off()
+plot(r_tree)
 
 
 
 
+# plotBranchbyTrait(b_tree, bfam$mean, # x2 variable used to colour
 
-# plotBranchbyTrait(b_tree, bfam$mean, 
 #                   type = "fan", edge.width=0.5, cex=0.4, show.tip.label=FALSE,
 #                   mode='tips', 
 #                   palette=palette, legend=TRUE)#viridis
@@ -336,12 +264,8 @@ dev.off()
 
 
 
-# reptiles ####
-r_tree <- ape::read.tree(file = "Data/rep_phyl_Pyron.txt")
-r_tree 
+## amphibians ####
 
-plot(r_tree)
 
-# x2 variable used to colour
 
 
