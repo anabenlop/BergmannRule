c5fd22189b1cd7eafa822447375c1864ea602b1d
anabenlop <abenitez81@gmail.com>
2022-05-31 17:43:39 +0200

    save phylogeny datasets
    Needed to test taxonomic coverage

--- BergmannsRule/Scripts/11_CircularBarplot_data coverage.R
+++ BergmannsRule/Scripts/11_CircularBarplot_data coverage.R
@@ -1,4 +1,15 @@
-# library
+##############################################################
+# Authors: 
+# Ana Benítez
+# Email: abenitez81@gmail.com
+
+# Script to check family coverage in the dataset 
+# Made on 31 May 2022
+
+# clean environment
+rm(list = ls())
+
+# load libraries
 library(tidyverse)
 library(viridis)
 library(ggplot2)
@@ -6,7 +17,11 @@ library(tidyr)
 library(ggpubr)
 library(scales)
 library(dplyr)
+library(ape)
+library(ggtree)
+library(phytools)
 
+# Phylogenetic trees for the four taxa ####
 # mammals ####
 ET<-read.csv('Data/EltonTraits_Mammals_taxid.csv', stringsAsFactors = F)
 ET<-ET[!ET$Order %in% c('Sirenia'),]
@@ -119,26 +134,111 @@ ggarrange(p2, p1, labels=c('(a)', '(b)'), font.label=list(size=20, face='plain')
           nrow=2, ncol=2)
 dev.off()
 
-# birds ####
-ET<-read.csv('Data/BirdFuncDat.csv', stringsAsFactors = F)
-ET<-ET[!ET$PelagicSpecialist %in% 1,] # remove Pelagic species
-ET<-ET[!ET$BLFamilyLatin %in% 'Pelecanidae',]
+## birds ####
+# load AVONET Datase BirdLife taxonomy
+avonet<-read.csv('Data/AVONET1_BirdLife.csv', stringsAsFactors = F)
 
+# habfam <- avonet %>% 
+#           group_by(Habitat, Family1) %>%
+#           summarize(freq = n())
+
+# marine <- habfam %>% 
+#           filter(Habitat == "Marine")
+
+fam <- avonet %>% 
+      group_by(Family1) %>%
+      summarize(freq = n()) # 11009 species
+
+# load tree - Tobias et al. 2022 - AVONET - BirdLife
+b_tree <- ape::read.nexus(file = "Data/Birdlife_Family_Phylogeny.nex")
+b_tree
+plot(b_tree)
+
+# load data bergmann
 birddata <- read.csv('Data/birddata_ph.csv', stringsAsFactors = F)
-birddata <- birddata[birddata$env.var == 'tavg', ]
+
+# summarize by env.var and family 
+birddata2 <- birddata %>% 
+  group_by(env.var, family) %>%
+  summarise(mean = mean(corr.coeff),
+            nsp = n())
+
+# use data for tavg only - discrete plot
+bird_tavg <- birddata2 %>% 
+  filter(env.var == "tavg") # 1545 species
+
+# create dataframe with all described families 
+bfam <- data.frame(family = b_tree$tip.label)
+
+# remove families with marine species from AVONET and tree
+bfam <- bfam[!bfam$family %in% c("Alcidae","Chionidae", "Diomedeidae", "Fregatidae", "Hydrobatidae", "Spheniscidae", 'Pelecanidae', "Procellariidae", "Phaethontidae"),]
+bfam <- data.frame(family = bfam)
+b_tree2 <- drop.tip(b_tree, tip = c("Alcidae","Chionidae", "Diomedeidae", "Fregatidae", "Hydrobatidae", "Spheniscidae", 'Pelecanidae', "Procellariidae", "Phaethontidae"))
+
+# join dataset with families included in Bergmann's rule paper
+bfam <- left_join(bfam, bird_tavg, by = "family")
+bfam$inc <- ifelse(is.na(bfam$mean), "no", "yes")
+bfam$hyp <- ifelse(bfam$mean < 0, "yes","no")
+
+# palette <- c("yes" = "dark purple", "no" = "yellow")
+
+p <- ggtree(b_tree2, layout = "circular") 
+p %<+% bfam + geom_tree(aes(color = inc), size = 1) + geom_tiplab(aes(color=inc)) +
+  scale_color_manual(values= wes_palette("Cavalcanti1", n = 2)) 
+
+ggsave("Figures/phylo_birds.png", 
+       width= 250, height= 250, units = 'mm', dpi=300)
+
+
+# exclude families in the tree that are not in dataset
+bfam2 <- bfam[bfam$bin == 1,]
+drops <- b_tree2$tip.label[!b_tree2$tip.label %in% bfam2$family]
+b_tree3 <- drop.tip(b_tree2, drops)
+
+# create tree
+p2 <-  ggtree(b_tree3, layout = "circular") 
+
+# nsp per family
+p2 %<+% bfam2 + geom_tree(aes(color = log10(nsp)), size = 1) + geom_tiplab(aes(color = log10(nsp))) +
+        scale_color_viridis() 
+
+# mean corr coeff tavg per family
+p2 %<+% bfam2 + geom_tree(aes(color = mean), size = 1) + geom_tiplab(aes(color = mean)) +
+  scale_color_viridis() 
+
+# Adherence to heat conserv hypothesis birds (bergmann's rule)
+p2 %<+% bfam2 + geom_tree(aes(color = hyp), size = 1) + geom_tiplab(aes(color = hyp)) +
+         scale_color_manual(values= wes_palette("Cavalcanti1", n = 2)) 
+
+table(bfam2$bergmann) # 104 families follow bergmann's rule
+
+
+# phylogenetic clustering
+
+bfam$bin <- ifelse(bfam$inc == "yes", 1, 0)
+bfam$bin <- as.integer(bfam$bin)
+
+bfam_D <- comparative.data(b_tree2, bfam, family) # does not work, it only shows families included in the analysis.
+PhyloD <- phylo.d(bfam_D, binvar=bin)
+
+# reptiles ####
+ET<-read.csv('Data/reptile_checklist_2022_03.csv', stringsAsFactors = F)
+
+reptdata <- read.csv('Data/reptdata_ph.csv', stringsAsFactors = F)
+reptdata <- reptdata[reptdata$env.var == 'tavg', ]
 
 ET2<-ET[!duplicated(ET$BLFamilyLatin),] # 184 families
-birddata2<-birddata[!duplicated(birddata$family),] # 140 families (140/184*100 = 76%)
+reptdata2<-reptdata[!duplicated(reptdata$family),] # 140 families (140/184*100 = 76%)
 
 TabET<-table(ET$IOCOrder) #n species
 TabETf<-table(ET2$IOCOrder) #n families
-TabSP<-table(birddata$order) #n species
-TabSPf<-table(birddata2$order) #n families
+TabSP<-table(reptdata$order) #n species
+TabSPf<-table(reptdata2$order) #n families
 
 ET<-data.frame(Order=names(TabET), Nfm=as.numeric(TabETf), Nsps=as.numeric(TabET))
-bird_dt<-data.frame(Order=names(TabSP), NfmInSamp=as.numeric(TabSPf), NspInSamp= as.numeric(TabSP))
+rept_dt<-data.frame(Order=names(TabSP), NfmInSamp=as.numeric(TabSPf), NspInSamp= as.numeric(TabSP))
 
-data<-left_join(ET, bird_dt, by = 'Order')
+data<-left_join(ET, rept_dt, by = 'Order')
 data$NspInSamp[is.na(data$NspInSamp)]<-0
 data$NfmInSamp[is.na(data$NfmInSamp)]<-0
 
@@ -219,9 +319,29 @@ p2<-ggplot(data2) +
 p2
 
 #SAVE ALL
-pdf('Figures/FamilyCoverageBirds.pdf', width=18, height=12)#width=13, height=8)
+pdf('Figures/FamilyCoverageRept.pdf', width=18, height=12)#width=13, height=8)
 ggarrange(p2, p1, labels=c('(a)', '(b)'), font.label=list(size=20, face='plain'),
           nrow=2, ncol=2)
 dev.off()
+
+
+
+
+
+# plotBranchbyTrait(b_tree, bfam$mean, 
+#                   type = "fan", edge.width=0.5, cex=0.4, show.tip.label=FALSE,
+#                   mode='tips', 
+#                   palette=palette, legend=TRUE)#viridis
+
+
+
+
+# reptiles ####
+r_tree <- ape::read.tree(file = "Data/rep_phyl_Pyron.txt")
+r_tree 
+
+plot(r_tree)
+
+# x2 variable used to colour
 
 
