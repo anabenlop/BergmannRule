d2dc2d6b8310e98804187aa810adf4234629d9a4
anabenlop <abenitez81@gmail.com>
2022-09-15 11:35:31 +0200

    update phylogeny scripts

--- BergmannsRule/Scripts/03_phylogeny_reptiles.R
+++ BergmannsRule/Scripts/03_phylogeny_reptiles.R
@@ -1,22 +1,16 @@
 ##############################################################
 # Authors: 
-# Ana Benitez-Lopez (@anabenlop)
+# Erin Henry, Ana Benitez-Lopez (@anabenlop)
+# Email: erinhenry55@gmail.com, abenitez81@gmail.com, ana.benitez@ugr.es
 # Scholar Profile: https://scholar.google.com/citations?user=HC_j51sAAAAJ&hl=es
-# Department of Integrative Ecology, Estacion Biologica de Donana (EBD-CSIC, ESP) 
-# Email: abenitez81@gmail.com
-
-# Script first created on the 27th of December 2021
+# https://www.anabenitezlopez.com/
 
 ##############################################################
 # Description of script and instructions
 ##############################################################
 
-# This script is used to build the phylogeny for reptiles and amphibians (herps) and estimate the 
-# phylogenetic relatedness among the species to test differences between thermoconformers
-# and thermoregulatos for the paper:
-
-# Henry, E., Santini, L., Huijbregts, M. A. J., Benítez-López, A. Uncovering the environmental drivers 
-# of intraspecific body size variation in terrestrial vertebrates. 
+# This script is used to build the phylogeny for reptiles and estimate the 
+# phylogenetic relatedness among the species
 
 ##############################################################
 # Packages needed
